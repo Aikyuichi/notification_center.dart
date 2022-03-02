6bc0dfc1987d23022ba626affb8c288ae1562b95
Aikyuichi <aikyu.sama@gmail.com>
2022-03-02 18:56:20 -0500

    Initial commit

---
+++ lib/notification_center.dart
@@ -0,0 +1,49 @@
+library notification_center;
+
+class NotificationCenter {
+  final _observers = <String, List<Function>>{};
+
+  static final NotificationCenter _instance = NotificationCenter.internal();
+
+  factory NotificationCenter() => _instance;
+
+  NotificationCenter.internal();
+
+  void subscribe(String notificationId, Function callback) {
+    if (!_observers.containsKey(notificationId)) {
+      _observers[notificationId] = [];
+    }
+    _observers[notificationId]?.add(callback);
+  }
+
+  void unsubscribe(String notificationId, {Function? callback}) {
+    if (callback != null) {
+      if (_observers.containsKey(notificationId)) {
+        _observers[notificationId]!.remove(callback);
+      }
+    } else {
+      _observers.remove(notificationId);
+    }
+  }
+
+  void notify(String notificationId, {NotificationData? data}) {
+    if (_observers.containsKey(notificationId)) {
+      final observer = _observers[notificationId]!;
+      if (data != null) {
+        for (var callback in observer) {
+          callback(data);
+        }
+      } else {
+        for (var callback in observer) {
+          callback();
+        }
+      }
+    }
+  }
+}
+
+class NotificationData {
+  final dynamic value;
+
+  NotificationData(this.value);
+}
